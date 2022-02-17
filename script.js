cb8ed9ad42ac7b40a0f712f2bc67d9323b1ca446
calopessoa <nashlasterby@gmail.com>
2022-02-17 10:12:06 -0300

    Adicionado mais cores para opçoes de placas

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const placa = document.querySelector('.placa');
 const inputText = document.querySelector('#input-text');
+const plaqueType = document.querySelector('#plaque-type');
 
 inputText.addEventListener('keyup', () => placa.innerText = inputText.value);
 
-const download = function() {
-    let link = document.createElement('a');
-    link.download = 'filename.png';
-    link.href = document.querySelector('.placa').toDataURL()
-    link.click();
+plaqueType.addEventListener('change', () => {
+  const board = plaqueType.value;
+  const afterStyle = document.head.appendChild(document.createElement("style"));
+  if (board === 'blue') {
+    placa.style.backgroundColor = '#006eb0';
+    afterStyle.innerHTML = ".placa:after {background: #006eb0;}";
+  } else if (board === 'green') {
+    placa.style.backgroundColor = '#4a9242';
+    afterStyle.innerHTML = ".placa:after {background: #4a9242;}";
+  } else if (board === 'red') {
+    placa.style.backgroundColor = '#ef482a';
+    afterStyle.innerHTML = ".placa:after {background: #ef482a;}";
   }
+});
